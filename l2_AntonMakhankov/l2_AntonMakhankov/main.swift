2a85a54d891cc2501873cf288797a5f9daf80074
Mahankov <anton.mahankov@yandex.ru>
2019-03-02 02:14:13 +0300

    Домашнее задание №2

---
+++ l2_AntonMakhankov/l2_AntonMakhankov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l2_AntonMakhankov
+//
+//  Created by Anton Makhankov on 02/03/2019.
+//  Copyright © 2019 Anton Makhankov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
