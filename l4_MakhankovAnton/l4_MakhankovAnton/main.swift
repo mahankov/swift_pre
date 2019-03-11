eae8bba1404e95b2b249b2792245310d7708a6db
Mahankov <anton.mahankov@yandex.ru>
2019-03-11 10:24:36 +0300

    Выполнена домашняя работа к 4 уроку

---
+++ l4_MakhankovAnton/l4_MakhankovAnton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l4_MakhankovAnton
+//
+//  Created by Anton Makhankov on 10/03/2019.
+//  Copyright © 2019 Anton Makhankov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
