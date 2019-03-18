03323d6dcf2e5d01f5ee443085c81a91d22a307f
Mahankov <anton.mahankov@yandex.ru>
2019-03-18 18:04:21 +0300

    Выполнена 6-я домашняя работа

---
+++ l6_MakhankovAnton/l6_MakhankovAnton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l6_MakhankovAnton
+//
+//  Created by Anton Makhankov on 18/03/2019.
+//  Copyright © 2019 Anton Makhankov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
