28f973a43d30057705ac5726b7f8e05596c96df2
Mahankov <anton.mahankov@yandex.ru>
2019-03-19 16:24:55 +0300

    Выполнена 7-я домашняя работа

---
+++ l7_MakhankovAnton/l7_MakhankovAnton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l7_MakhankovAnton
+//
+//  Created by Anton Makhankov on 19/03/2019.
+//  Copyright © 2019 Anton Makhankov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
